add180d04b62d86d3462aa6827892f41ca2082d4
Konrad Korpik <337303@uwr.edu.pl>
2023-01-04 12:55:48 +0100

    Lista 7

--- Lista 7/zad3.py
+++ Lista 7/zad3.py
@@ -1,9 +1,7 @@
 import random
 import time
-import zadanie2
+import zad2
 
-
-# zbior = [20,1, 5, 2, 9, 14, 8, 10, 19, 4, 5, 1, 0, 15, 12, 16, 4]
 def bombelki(zbior):
     for i in range(0, len(zbior) - 1):
         for j in range(0, len(zbior) - 1):
@@ -22,4 +20,4 @@ for i in range(100, 400, 100):
     print(bombelki(zbior))
     print("bombelkowy czas= ", t2 - t1)
 
-print(zadanie2)
+print(zad2)
